a66e6631f4da8cbb75306b696a48a78512cbf7da
duriyudha <duriyudha@gmail.com>
2026-02-06 10:26:02 +0900

    Add user input and verbose args, add message history structure TBD

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY is not set in environment variables.")
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     client = genai.Client(api_key=api_key)
-    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=user_prompt,
+        contents=messages
     )
-    print(f"User prompt: {user_prompt}\n")
-    if not response.usage_metadata:
-        raise RuntimeError("Usage metadata is missing in the response likely due to a failed API call.")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}\n")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+        if not response.usage_metadata:
+            raise RuntimeError("Usage metadata is missing in the response likely due to a failed API call.")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}\n")
+    
     print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
