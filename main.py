473128239dc18551faaee26443332331da5ba2c2
duriyudha <duriyudha@gmail.com>
2026-02-06 10:06:34 +0900

    Initial commit: AI agent project

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY is not set in environment variables.")
+    client = genai.Client(api_key=api_key)
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=user_prompt,
+    )
+    print(f"User prompt: {user_prompt}\n")
+    if not response.usage_metadata:
+        raise RuntimeError("Usage metadata is missing in the response likely due to a failed API call.")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}\n")
+    print(f"Response:\n{response.text}")
+
+if __name__ == "__main__":
+    main()
