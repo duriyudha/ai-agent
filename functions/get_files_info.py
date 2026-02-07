c2b0d4e336033495d41f575203ea0ec1034aed7d
duriyudha <duriyudha@gmail.com>
2026-02-07 17:02:40 +0900

    Add get_file_content and get_files_info

---
+++ functions/get_files_info.py
@@ -0,0 +1,22 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    if not valid_target_dir:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+    
+    try:
+        result = "Result for current directory:\n" if directory == "." else f"Result for '{directory} directory:\n"
+        for item in os.listdir(target_dir):
+            item_path = os.path.join(target_dir, item)
+            size = os.path.getsize(item_path)
+            is_dir = os.path.isdir(item_path)
+            result += f"- {item}: file_size={size} bytes, is_dir={is_dir}\n"
+        return result.strip()
+    except Exception as e:
+        return f"Error: {e}"
