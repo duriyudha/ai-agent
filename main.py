e7429ebc4c4f03d0f33257776fddf9ed545d071f
duriyudha <duriyudha@gmail.com>
2026-02-09 18:12:25 +0900

    Add system prompts template

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import argparse
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+
+from prompts import system_prompt
 
 def main():
     load_dotenv()
@@ -17,9 +20,12 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     client = genai.Client(api_key=api_key)
+    model_name = "gemini-2.5-flash"
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages
+        model=model_name,
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt,
+                                           temperature=0)
     )
     if args.verbose:
         print(f"User prompt: {args.user_prompt}\n")
